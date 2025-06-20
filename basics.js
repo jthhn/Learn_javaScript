d9738a735c62a8db5edc4be89ab3e080a2e12a82
jthhn <jithinam530@gmail.com>
2025-06-20 19:30:17 +0530

    learning..

---
+++ basics.js
@@ -0,0 +1,94 @@
+// console.log("Hello world")
+
+// function summ(...args){
+//     for (let arg in args){
+//         console.log(args[arg])
+//     }
+// }
+// console.log(summ(4,2,5,2,1,5))
+
+
+// let a = 'b'
+// console.log(a)
+// a = 5
+// console.log(a)
+// if (a == 5){
+//     let a = 6
+//     console.log(a)
+// }
+// console.log(a)
+
+// let a = BigInt("543")
+// let b = BigInt('454')
+// let c = a + b
+// console.log(c)
+// console.log(typeof c)
+// console.log(typeof b)
+// console.log(typeof 5)
+
+
+// let d
+// console.log(typeof d)
+// let e = null;
+// console.log(typeof e)
+
+// const item = {
+//     "Jithin": true,
+//     "Shubh": false,
+//     "Lovish": 67,
+//     "Goutham": undefined
+// }
+// console.log(item['Jithin'])
+// console.log(item['Lovish'])
+// console.log(item['Goutham'])
+
+
+// let a = "Jithin"
+// let b = 6
+// console.log(typeof (a + b))
+
+// const a1 = {
+//     name: 'jithin',
+//     'age': 23,
+//     'martial status': 'Single'
+// }
+
+// a1['name'] = 'NewName'
+// console.log(typeof(a1.name))
+// 12
+// 34
+// 23 
+
+// let a = 
+// console.log(typeof a);
+// if (a>10){
+//     console.log('This is valid');
+// }
+// else{
+//     console.log('This is an invalid age');
+// }
+
+
+// let age = 22;
+// if (age>10 && age<20){
+//     console.log('Your age lies between 10 and 20');
+// }
+// else{
+//     console.log('Your age doesn"t lies between 10 and 20');
+// }
+
+// let age = 22;
+// switch(age){
+//     case 12:
+//         console.log("Your age is 12");  
+//         break;
+//     case 22:
+//         console.log("Your age is 22");
+//         break;
+//     case 2:
+//         console.log("Your age is too low to end up here. Watch cocomelon.");
+//         break;
+//     default:
+//         console.log('Learn javscript.');
+// }
+
