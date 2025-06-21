a13bed2f89890d0410d9a421019590c3e2a99244
jthhn <jithinam530@gmail.com>
2025-06-21 22:05:24 +0530

    Learning..

--- basics.js
+++ basics.js
@@ -134,3 +134,122 @@
 //     console.log(i);
 //     i++;
 // }
+
+// function onePlusAvg(x,y) {
+//     return 1 + (x + y) /2
+// }
+// console.log(onePlusAvg(4,8))
+
+// const hello = () =>{
+//     console.log("Hey how are you?")
+// }
+// hello()
+
+
+// let marks = {
+//     haryy: 23,
+//     jithin: 99,
+//     lovish: 55,
+//     Monika: 12,
+// };
+// for (let i = 0;  i<Object.keys(marks).length; i++){
+//     console.log(i)
+//     console.log('The marks of ' + Object.keys(marks)[i] + " are " + marks[Object.keys(marks)[i]])
+// }
+
+// for (let i in marks) {
+//     console.log(i)
+
+// }
+
+// let cn = 4
+// let i = 4
+// while (i != cn){
+//     cosole.log('Try again')
+//     i = 
+// }
+
+// const mean = (a, b, c, d) => {
+//     return (a + b + c + d)
+// }
+
+// console.log(mean(3,4,5,2)/4)
+
+
+// n = 'string'
+// console.log(n.length)
+
+// b = "string"
+// console.log(b === n)
+
+// let name = 'jithin'
+// console.log(name[0])
+// console.log(name[3])
+// let friend = 'Jithinam'
+// console.log(friend[4])
+
+// let boy1 = 'jithin'
+// let boy2 = 'jtihin2'
+// let sentence = `${boy1} is a friend of ${boy2}`
+// console.log(sentence)
+
+// let name = 'jithin\''
+// console.log(name.length)
+// let name = 'Jithin'
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+
+// console.log(name.slice(3,5))
+
+// let fr = "shivika";
+// console.log(fr[0])
+// console.log(fr[1])
+// console.log(fr[2])
+// console.log(fr[3])
+// console.log(fr[4])
+
+// for (let i = 0;i <= fr.length; i++){
+//     console.log(fr[i]);
+// }
+
+// const sentence = 'The quick brown fox jumps over the lazy dog.';
+// const word = 'fox';
+// console.log(`The word "${word}" ${sentence.includes(word)? 'is' : 'is not'} in the senctence.`);
+
+
+
+// let str2 = "Please give Rs 1000"
+// let length = Number.parseInt('Please give Rs'.length)
+// let amount = str2.slice('Please give Rs'.length)
+
+// console.log(amount)
+// console.log(typeof amount)
+
+
+// let friend = "Deepika"
+// friend[2] = "R"
+// console.log(friend)
+
+// let marks_class_12 = [1, 2, 34, 5, 3, null, false, "string", undefined]
+// console.log(marks_class_12)
+
+
+// let num = [1, 2, 34, 5, 5, 3, 9]
+// let b = num.toString()
+// console.log(typeof b)
+
+// let num = [1, 3, 4, 9]
+// let numm = [1, 3, 4, 9]
+// let newArray = num.concat(numm)
+// console.log(newArray)
+
+// const num = [1, 3,8, 7, 4, 9];
+// let a = num.sort();
+// console.log(num);
+
+// let compare = (a,b) =>{
+//     return a-b;
+// }
+// let num = [551, 22, 3, 14, 5, 6, 7, 8, 299]
+// num.sort(compare)
+// console.log(num)
